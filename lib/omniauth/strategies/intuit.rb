ee65abf909ed61c9abc9fcace5039f0b69dace57
Kyle Daigle <kyle@digitalworkbox.com>
2012-04-30 18:52:01 -0400

    Actually set the UID and info

--- lib/omniauth/strategies/intuit.rb
+++ lib/omniauth/strategies/intuit.rb
@@ -12,41 +12,35 @@ module OmniAuth
         :authorize_url => "https://workplace.intuit.com/Connect/Begin"
       }
 
-      # uid{ raw_info['id'] }
+      uid{ request.params['realmId'] }
 
-      # info do
-      #         {
-      #           :first_name => raw_info['firstName'],
-      #           :last_name => raw_info['lastName'],
-      #           :name => "#{raw_info['firstName']} #{raw_info['lastName']}",
-      #           :email => raw_info['email'],
-      #           :data_source => raw_info['dataSource'],
-      #           :oauth_verifier => raw_info['oauth_verifier'],
-      #           :realm_id => raw_info['realmId']
-      #
-      #         }
-      #       end
-      #
-      #       extra do
-      #         { 'raw_info' => raw_info }
-      #       end
-      #
-      #       def raw_info
-      #         # access_token.get(path, headers={})
-      #         # dataSource
-      #         #         "QBO"
-      #         #         oauth_token
-      #         #         "qyprdhjNbeM7UGDHYrgSvwqCRUQP0nZejdHw3IIFTaHY8mj5"
-      #         #         oauth_verifier
-      #         #         "ee3njpe"
-      #         #         realmId
-      #         #         "313247180"
-      #
-      #         puts "==================="
-      #         # access_token.get("/auth/intuit/callback").body
-      #         puts "==================="
-      #         @raw_info ||= Crack::XML.parse(access_token.get("").body)["RestResponse"]
-      #       end
+      info do
+        {
+          :data_source => request.params['dataSource'],
+          :realm_id => request.params['realmId']
+        }
+      end
+      
+      # extra do
+      #   { 'raw_info' => raw_info }
+      # end
+      
+      # def raw_info
+      #   # access_token.get(path, headers={})
+      #   # dataSource
+      #   #         "QBO"
+      #   #         oauth_token
+      #   #         "qyprdhjNbeM7UGDHYrgSvwqCRUQP0nZejdHw3IIFTaHY8mj5"
+      #   #         oauth_verifier
+      #   #         "ee3njpe"
+      #   #         realmId
+      #   #         "313247180"
+      # 
+      #   puts "==================="
+      #   puts access_token.get("/auth/intuit/callback").body
+      #   puts "==================="
+      #   @raw_info ||= Crack::XML.parse(access_token.get("/auth/intuit/callback").body)["RestResponse"]
+      # end
     end
   end
 end
